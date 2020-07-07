6afb51d9629e3d6467e2ef7deb12aa916fd46874
Barry Nouwt <barry.nouwt@tno.nl>
2020-07-07 13:31:19 +0200

    Improved the example, but now it no longer fails...

--- src/main/java/nl/tno/ict/ds/cb/jena/JenaPathTest.java
+++ src/main/java/nl/tno/ict/ds/cb/jena/JenaPathTest.java
@@ -6,6 +6,6 @@ public class JenaPathTest {
 	public static void main(String[] args) {
 
 		FusekiServer.Builder builder = FusekiServer.create();
-		builder.parseConfigFile("src/test/resources/config1.ttl");
+		builder.parseConfigFile("src/test/resources/config.ttl");
 	}
 }
