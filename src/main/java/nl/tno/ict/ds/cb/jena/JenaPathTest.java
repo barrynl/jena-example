c4d4dbcbbe0da905009bc2adcd243dea4c269bf6
Barry Nouwt <barry.nouwt@tno.nl>
2020-07-07 13:22:23 +0200

    Setup test case for: https://jena.markmail.org/thread/folnxzuvbeqel5bn

---
+++ src/main/java/nl/tno/ict/ds/cb/jena/JenaPathTest.java
@@ -0,0 +1,11 @@
+package nl.tno.ict.ds.cb.jena;
+
+import org.apache.jena.fuseki.main.FusekiServer;
+
+public class JenaPathTest {
+	public static void main(String[] args) {
+
+		FusekiServer.Builder builder = FusekiServer.create();
+		builder.parseConfigFile("src/test/resources/config1.ttl");
+	}
+}
